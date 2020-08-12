fe5a7700f112b43c57e392c92de6a518d0e9257a
sedx876 <swatkins876@gmail.com>
2020-08-12 09:35:18 -0400

    Added styling and component files

---
+++ src/Video.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <h1>This might be a video</h1>
+    </div>
+  )
+}
+
+export default Video
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB68hRd5Bi0KJeS2szkkuR7I7ewA38YE-4",
+  authDomain: "klonetok.firebaseapp.com",
+  databaseURL: "https://klonetok.firebaseio.com",
+  projectId: "klonetok",
+  storageBucket: "klonetok.appspot.com",
+  messagingSenderId: "459099258790",
+  appId: "1:459099258790:web:24598904542689ab6ad967",
+  measurementId: "G-NQ8GRMMHEQ"
+};
