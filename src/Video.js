fdad3fe6f145ba234d157a2df8233c44306247b8
sedx876 <swatkins876@gmail.com>
2020-08-12 10:07:02 -0400

    setup video player

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import "./Video.css";
 
 function Video() {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef(null);
+
+  const onVideoPress = () => {
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
+  };
+
   return (
     <div className="video">
       <h1>This might be a video</h1>
+      <video className="video__player" 
+      loop
+      onClick={onVideoPress}
+      ref={videoRef}
+      src="">
+
+      </video>
     </div>
   )
 }
